d856abdfaa73f8c83cadfb2852358fe8f398d9c4
Nguyen Tuong Van <van.tuong.nguyen@hotmail.com>
2019-08-20 18:02:00 +0800

    Fix Shichika

---
+++ src/pages/art/shichika-yasuri/Shichika.js
@@ -0,0 +1,298 @@
+import React from "react";
+import './Shichika.css';
+export default function Shichika() {
+  return <div class="playground-shichika">
+  <div class="shichika">
+    <div class="hair">
+      <div class="hair-shine-frame"><div class="hair-shine"></div></div>
+      <div class="hair-lock hair-lock-1">
+        <div class="spike-1"></div>
+        <div class="spike-2"></div>
+        <div class="spike-3"></div>
+      </div>
+      <div class="hair-lock hair-lock-2">
+        <div class="spike-4"></div>
+        <div class="spike-5"></div>
+        <div class="spike-6"></div></div>
+      <div class="hair-lock hair-lock-3">
+        <div class="spike-4"></div>
+        <div class="spike-5"></div>
+        <div class="spike-6"></div>
+      </div>
+      <div class="hair-lock hair-lock-4"></div>
+      <div class="hair-lock hair-lock-5">
+        <div class="spike-4"></div>
+        <div class="spike-5"></div>
+        <div class="spike-6"></div>
+      </div>
+      <div class="hair-lock hair-lock-6">
+        <div class="spike-7"></div>
+        <div class="spike-8"></div>
+        <div class="spike-9"></div>
+      </div>
+      <div class="hair-top-1"></div>
+      <div class="hair-top-2"></div>
+    </div>
+    <div class="hair-tail">
+       <div class="hair-maple"><div class="maple-leaf">
+            <div class="part-1 part"></div>
+            <div class="part-2 part"></div>
+            <div class="part-3 part"></div>
+            <div class="part-4 part"></div>
+            <div class="part-5 part"></div>
+            <div class="part-6 part"></div>
+            <div class="part-7 part"></div>
+          </div>
+         </div>
+       <div class="tail-top">
+         <div class="tail-2"></div>
+         <div class="tail-3"></div>
+         <div class="tail-4"></div>
+         <div class="tail-5"></div>
+         <div class="tail-6"></div>
+         <div class="tail-7"></div>
+      </div>
+    </div>
+    <div class="face">
+      <div class="upper-half">
+        <div class="eye"><div class="left lash"></div><div class="left brow"></div></div>
+        <div class="nose"></div>
+        <div class="eye">
+          <div class="right lash"></div>
+          <div class="right brow"></div>
+        </div>
+      </div>
+      <div class="mouth"></div>
+      <div class="left-ear"></div>
+      <div class="right-ear"></div>
+    </div>
+    <div class="neck">
+      <div class="left-line"><div class="top"></div><div class="bottom"></div></div>
+      <div class="right-line"></div>
+    </div>
+    <div class="left-shoulder">
+      <div class="left-shoulder-shade"></div>
+      <div class="neck-bg">
+        <div class="neck-bg-upper"></div>
+        <div class="neck-bg-lower"></div>
+        <div class="neck-bg-lower-2"></div>
+        <div class="neck-bg-lower-3"></div>
+      </div>
+    </div>
+    <div class="left-arm-upper">
+      <div class="left-arm-lower-1"></div>
+      <div class="left-arm-lower-2"></div>
+    </div>
+    <div class="gauntlet-left">
+      <div class="gauntlet-left-1"></div>
+      <div class="gauntlet-left-shade"></div>
+    </div>
+    <div class="right-shoulder"></div>
+    <div class="neck-bone"></div>
+    <div class="left collar-bone"></div>
+    <div class="right collar-bone">
+      <div class="left"></div>
+      <div class="right"></div>
+    </div>
+    <div class="chest">
+      <div class="left">
+        <div class="top"></div>
+        <div class="bottom">
+          <div class="shade-1"></div>
+        </div>
+      </div>
+      <div class="center"></div>
+      <div class="right"></div>
+    </div>
+    <div class="chest-bg">
+      <div class="chest-bg-shadow"></div>
+      <div class="stomach-shade"></div>
+      <div class="stomach-shade-2"></div>
+      <div class="stomach-shade-3"></div>
+      <div class="stomach-shade-4"></div>
+    </div>
+    <div class="right-arm">
+      <div class="right-arm-upper">
+        <div class="bottom">
+            <div class="biceps">
+              <div class="right-arm-shadow-2">
+                <div class="right-arm-shadow-1"></div>
+              </div>
+              <div class="right-arm-shadow-3"></div><div class="elbow"><div class="right-arm-shadow-4"></div><div class="right-arm-shadow-5"></div></div></div>
+        </div>
+      </div>
+      <div class="right-arm-gauntlet">
+        <div class="right-arm-gauntlet-upper"></div>
+        <div class="right-arm-gauntlet-lower"></div>
+        <div class="shading-upper"></div>
+        <div class="shading-lower"></div>
+      </div>
+      <div class="right-palm">
+         <div class="palm-maple"><div class="maple-leaf">
+            <div class="part-1 part"></div>
+            <div class="part-4 part"></div>
+            <div class="part-5 part"></div>
+            <div class="part-6 part"></div>
+            <div class="part-7 part"></div>
+          </div>
+         </div>
+        <div class="thumb">
+          <div class="thumb-upper"><div class="thumb-lower"></div></div>
+        </div>
+        <div class="palm-upper">
+          <div class="fingers">
+            <div class="finger-1">
+              <div class="top"><div class="bottom"></div></div>
+            </div>
+            <div class="finger-2">
+              <div class="top"><div class="bottom"></div></div>
+            </div>
+            <div class="finger-3">
+              <div class="top"><div class="bottom"></div></div>
+            </div>
+            <div class="finger-4">
+              <div class="top"><div class="bottom"></div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="right-rib"></div>
+    <div class="right-rib-shadow"></div>
+    <div class="belly-shadow-left"></div>
+    <div class="belly-shadow-right"></div>
+    <div class="left-rib"></div>
+    <div class="lower-back"></div>
+    <div class="hip-left"></div>
+    <div class="hip-right"></div>
+    <div class="rib-center-top"></div>
+    <div class="rib-center-shadow"></div>
+    <div class="rib-center-bottom"></div>
+    <div class="belt"></div>
+    <div class="belt-1"></div>
+    <div class="belt-2"></div>
+    <div class="belt-shade-1"></div>
+    <div class="belt-3"></div>
+    <div class="pants">
+        <div class="nut-1 nut">
+          <div class="inner">
+          </div>
+          <div class="shine"></div>
+          <div class="shine-2"></div>
+          <div class="top"></div>
+          <div class="stalk"></div>
+          <div class="top-shadow"></div>
+        </div>
+        <div class="nut-2 nut">
+          <div class="inner">
+          </div>
+          <div class="shine"></div>
+          <div class="shine-2"></div>
+          <div class="top"></div>
+          <div class="stalk"></div>
+          <div class="top-shadow"></div>
+        </div>
+       <div class="nut-3 nut">
+          <div class="inner">
+          </div>
+          <div class="shine"></div>
+          <div class="shine-2"></div>
+          <div class="top"></div>
+          <div class="stalk"></div>
+          <div class="top-shadow"></div>
+        </div>
+        <div class="nut-4 nut">
+          <div class="inner">
+          </div>
+          <div class="shine"></div>
+          <div class="shine-2"></div>
+          <div class="top"></div>
+          <div class="stalk"></div>
+          <div class="top-shadow"></div>
+        </div>
+        <div class="nut-5 nut">
+          <div class="inner">
+          </div>
+          <div class="shine"></div>
+          <div class="shine-2"></div>
+          <div class="top"></div>
+          <div class="stalk"></div>
+          <div class="top-shadow"></div>
+        </div>
+       <div class="nut-6 nut">
+          <div class="inner">
+          </div>
+          <div class="shine"></div>
+          <div class="shine-2"></div>
+          <div class="top"></div>
+          <div class="stalk"></div>
+          <div class="top-shadow"></div>
+        </div>
+       <div class="pants-maple"><div class="maple-leaf">
+          <div class="part-1 part"></div>
+          <div class="part-2 part"></div>
+          <div class="part-3 part"></div>
+          <div class="part-4 part"></div>
+          <div class="part-5 part"></div>
+          <div class="part-6 part"></div>
+          <div class="part-7 part"></div>
+        </div>
+       </div>
+      <div class="upper"><div class="lower-1"></div></div>
+      <div class="lower-2"></div>
+      <div class="lower-3"></div>
+      <div class="lower-4"></div>
+      <div class="lower-5"></div>
+      <div class="right-foot foot"><div class="top"></div>
+        <div class="bottom">
+          <div class="shadow"></div>
+          <div class="toe-1"></div>
+          <div class="toe-2"></div>
+          <div class="toe-3"></div>
+          <div class="toe-4"></div>
+          <div class="toe-5"></div>
+        </div>
+       <div class="sandal-bottom"></div>
+       <div class="sandal-top">
+         <div class="left"></div><div class="right"></div>
+         <div class="sandal-maple"><div class="maple-leaf">
+            <div class="part-1 part"></div>
+            <div class="part-2 part"></div>
+            <div class="part-3 part"></div>
+            <div class="part-4 part"></div>
+            <div class="part-5 part"></div>
+            <div class="part-6 part"></div>
+            <div class="part-7 part"></div>
+          </div>
+         </div>
+        </div>
+      </div>
+      <div class="left-foot foot"><div class="top"></div>
+        <div class="bottom">
+          <div class="shadow"></div>
+          <div class="toe-1"></div>
+          <div class="toe-2"></div>
+          <div class="toe-3"></div>
+          <div class="toe-4"></div>
+          <div class="toe-5"></div>
+        </div>
+       <div class="sandal-bottom"></div>
+       <div class="sandal-top">
+         <div class="left"></div><div class="right"></div>
+         <div class="sandal-maple">
+           <div class="maple-leaf">
+            <div class="part-1 part"></div>
+            <div class="part-2 part"></div>
+            <div class="part-3 part"></div>
+            <div class="part-4 part"></div>
+            <div class="part-5 part"></div>
+            <div class="part-6 part"></div>
+            <div class="part-7 part"></div>
+          </div></div>
+       </div>
+      </div>
+    </div>
+    
+    </div>
+  </div>
+}
---
+++ src/pages/art/shichika-yasuri/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Shichika from './Shichika';
+
+function App() {
+  return <Shichika />;
+}
+
+export default App;
